cbc517b9d260d345861440703d8023bebfb01177
German-coder07 <gercuetol@gmail.com>
2025-10-07 12:28:59 -0600

    done part 1

---
+++ execute.py
@@ -0,0 +1,80 @@
+#Part 1 Find the 3 genes (M-gene, S-gene and ORF1AB-gene) in both sequence 
+# algorithm: KMP 
+
+def LPS(pattern):
+    List = [0] * len(pattern) 
+    L = 0  
+    R = 1  
+    while R < len(pattern):
+        if pattern[L] == pattern[R]:
+            List[R] = L + 1
+            R += 1
+            L += 1
+        else:
+            if L != 0:
+                L = List[L - 1]
+            else:
+                R += 1
+
+    return List
+
+def kmp(text, pattern):
+    i = 0  
+    j = 0  
+    lps = LPS(pattern)  
+
+    while i < len(text):
+        if text[i] == pattern[j]:
+            i += 1
+            j += 1
+        else:
+            if j > 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+        if j == len(pattern):
+            return i - j
+
+sars = ["SARS-COV-2-MN908947.3.txt","SARS-COV-2-MT106054.1.txt"]
+genes = ["M-gene.txt","S-gene.txt","ORF1AB-gene.txt"]
+
+SARS = []
+GENES = []
+
+for i in sars:  
+    f = open(i, "r")
+    lines = f.readlines()
+    lines = lines[1:]
+    f.close()
+    virus  = ""
+    for l in lines:
+        for c in l: 
+            if c != "\n":
+                virus = virus + c
+    SARS.append(virus)
+
+for i in genes:
+    f = open(i, "r")
+    lines = f.readlines()
+    lines = lines[1:]
+    f.close()
+    gene  = ""
+    for l in lines:
+        for c in l: 
+            if c != "\n":
+                gene = gene + c
+    GENES.append(gene)
+
+i = 0
+for virus in SARS:
+    j=0
+    print("Virus: "+sars[i])
+    i+=1
+    for gene in GENES:
+        print("Gene: "+genes[j])
+        print(kmp(virus,gene))
+        j+=1
+        
+
+
